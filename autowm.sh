511d22ba25c7280fd68d906b79b03def2647b920
lukas <lukas@kali.kali>
2024-07-03 00:32:16 -0700

    arreglo de script

--- autowm.sh
+++ autowm.sh
@@ -20,8 +20,12 @@ cd install
 
 
 echo -e  "Instalando Picom"
-git clone git clone https://github.com/ibhagwan/picom.git
+git clone https://github.com/ibhagwan/picom.git
 cd picom/
+sudo apt install -y cmake libconfig-dev libdbus-1-dev libegl-dev libev-dev libgl-dev libepoxy-dev libpcre2-dev libpixman-1-dev libx11-xcb-dev libxcb1-dev libxcb-composite0-dev libxcb-damage0-dev libxcb-glx0-dev libxcb-image0-dev libxcb-present-dev libxcb-randr0-dev libxcb-render0-dev libxcb-render-util0-dev libxcb-shape0-dev libxcb-util-dev libxcb-xfixes0-dev meson ninja-build uthash-dev
+sudo apt install libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-dev libxcb-render0-dev libxcb-randr0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev libxcb-xinerama0-dev libxcb-glx0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl1-mesa-dev libpcre2-dev libevdev-dev uthash-dev libev-dev libx11-xcb-dev libpcre3-dev
+
+
 git submodule update --init --recursive
 meson --buildtype=release . build
 ninja -C build
