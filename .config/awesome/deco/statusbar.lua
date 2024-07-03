5dfec774bfe20be3f70cfcf4d2bcb0b0e668ade5
lukas <lukas@kali>
2024-07-02 22:20:07 -0700

    Primer commit inicializando

---
+++ .config/awesome/deco/statusbar.lua
@@ -0,0 +1,262 @@
+-- Standard awesome library
+local gears = require("gears")
+local awful     = require("awful")
+
+-- Wibox handling library
+local wibox = require("wibox")
+
+-- Custom Local Library: Common Functional Decoration
+local deco = {
+  wallpaper = require("deco.wallpaper"),
+  taglist   = require("deco.taglist"),
+  tasklist  = require("deco.tasklist")
+}
+
+-- Custom widgets from awesome-wm-widgets
+-- https://github.com/streetturtle/awesome-wm-widgets.git
+
+local volume_widget = require('awesome-wm-widgets.volume-widget.volume')
+local brightness_widget = require("awesome-wm-widgets.brightness-widget.brightness")
+local batteryarc_widget = require("awesome-wm-widgets.batteryarc-widget.batteryarc")
+
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+local taglist_buttons  = deco.taglist()
+local tasklist_buttons = deco.tasklist()
+
+
+local _M = {}
+
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+
+-- {{{ Wibar
+-- Create a textclock widget
+mytextclock = wibox.widget.textclock(' %d/%m/%y %H:%M ')
+
+awful.screen.connect_for_each_screen(function(s)
+  -- Wallpaper
+  set_wallpaper(s)
+
+  -- Create a promptbox for each screen
+  s.mypromptbox = awful.widget.prompt()
+
+  -- Create an imagebox widget which will contain an icon indicating which layout we're using.
+  -- We need one layoutbox per screen.
+  s.mylayoutbox = awful.widget.layoutbox(s)
+  s.mylayoutbox:buttons(gears.table.join(
+    awful.button({ }, 1, function () awful.layout.inc( 1) end),
+    awful.button({ }, 3, function () awful.layout.inc(-1) end),
+    awful.button({ }, 4, function () awful.layout.inc( 1) end),
+    awful.button({ }, 5, function () awful.layout.inc(-1) end)
+  ))
+ -- aca se crea la caja y se puede moficar la forma
+  -- Create a taglist widget
+
+-- este se le pasa originalmente al layout
+--   s.mytaglist = awful.widget.taglist {
+--     screen  = s,
+--     filter  = awful.widget.taglist.filter.all,
+--     layout   = {
+--       spacing = 20,
+--       spacing_widget = {
+--           color  = '#dddddd',
+--           shape  = gears.shape.powerline,
+--           widget = wibox.widget.separator,
+          
+--       },
+--       layout  = wibox.layout.fixed.horizontal
+--   },
+
+--   widget_template = {
+--         {
+--             {
+--                 {
+--                     {
+--                         {
+--                             id     = 'index_role',
+--                             widget = wibox.widget.textbox,
+--                         },
+--                         margins = 4,
+--                         widget  = wibox.container.margin,
+--                     },
+--                     bg     = '#dddddd',
+--                     shape  = gears.shape.circle,
+--                     widget = wibox.container.background,
+--                 },
+--                 {
+--                     {
+--                         id     = 'icon_role',
+--                         widget = wibox.widget.imagebox,
+--                     },
+--                     margins = 2,
+--                     widget  = wibox.container.margin,
+--                 },
+--                 {
+--                     id     = 'text_role',
+--                     widget = wibox.widget.textbox,
+--                 },
+--                 layout = wibox.layout.fixed.horizontal,
+--             },
+--             left  = 0,
+--             right = 0,
+--             widget = wibox.container.margin,
+            
+--         },
+--         id     = 'background_role',
+--         widget = wibox.container.background,
+--         -- Add support for hover colors and an index label
+--         create_callback = function(self, c3, index, objects) --luacheck: no unused args
+--             self:get_children_by_id('index_role')[1].markup = '<b> '..index..' </b>'
+--             self:connect_signal('mouse::enter', function()
+--                 if self.bg ~= '#ff0000' then
+--                     self.backup     = self.bg
+--                     self.has_backup = true
+--                 end
+--                 self.bg = '#ff0000'
+--             end)
+--             self:connect_signal('mouse::leave', function()
+--                 if self.has_backup then self.bg = self.backup end
+--             end)
+--         end,
+--         update_callback = function(self, c3, index, objects) --luacheck: no unused args
+--             self:get_children_by_id('index_role')[1].markup = '<b> '..index..' </b>'
+--         end,
+    
+--   },
+ 
+-- }
+
+
+s.mytaglist = awful.widget.taglist {
+  screen  = s,
+  filter  = awful.widget.taglist.filter.all,
+--   style   = {
+--     shape = function(cr, width, height)
+--       gears.shape.powerline (cr, width, height, 10)
+--    end,
+--    spacing = -5,
+   
+-- },
+  layout   = {
+      spacing = 0,
+      spacing_widget = {
+          color  = '#8be9fd',
+          shape  = gears.shape.powerline,
+          widget = wibox.widget.separator,
+          
+      },
+      layout  = wibox.layout.fixed.horizontal
+  },
+  widget_template = {
+      {
+          {
+              {
+                  {
+                      {
+                          id     = 'index_role',
+                          widget = wibox.widget.textbox,
+                      },
+                      margins = -4,
+                      widget  = wibox.container.margin,
+                  },
+                  bg     = '#44475a',
+                  shape  = gears.shape.circle,
+                  widget = wibox.container.background,
+              },
+              {
+                  {
+                      id     = 'icon_role',
+                      widget = wibox.widget.imagebox,
+                  },
+                  margins = 1,
+                  widget  = wibox.container.margin,
+              },
+              {
+                  id     = 'text_role',
+                  widget = wibox.widget.textbox,
+              },
+              layout = wibox.layout.fixed.horizontal,
+          },
+          left  = 5,
+          right = 5,
+          widget = wibox.container.margin
+      },
+      id     = 'background_role',
+      widget = wibox.container.background,
+      
+    
+      -- Add support for hover colors and an index label
+      create_callback = function(self, c3, index, objects) --luacheck: no unused args
+          self:get_children_by_id('index_role')[1].markup = '<b> '..index..' </b>'
+          self:connect_signal('mouse::enter', function()
+              if self.bg ~= '#44475a' then
+                  self.backup     = self.bg
+                  self.has_backup = true
+              end
+              self.bg = '#44475a'
+          end)
+          self:connect_signal('mouse::press', function()
+            if self.bg ~= '#44475a' then
+                self.backup     = self.bg
+                self.has_backup = true
+            end
+            self.bg = '#44475a'
+        end)
+          self:connect_signal('mouse::leave', function()
+              if self.has_backup then self.bg = self.backup end
+          end)
+      end,
+      update_callback = function(self, c3, index, objects) --luacheck: no unused args
+          self:get_children_by_id('index_role')[1].markup = '<b> '..index..' </b>'
+      end,
+  },
+  buttons = taglist_buttons
+}
+
+  -- Create a tasklist widget
+  s.mytasklist = awful.widget.tasklist {
+    screen  = s,
+    filter  = awful.widget.tasklist.filter.currenttags,
+    buttons = tasklist_buttons
+  }
+
+  -- Create the wibox
+  s.mywibox = awful.wibar({ position = "top", screen = s ,height = 25, })
+
+  -- Add widgets to the wibox
+  s.mywibox:setup {
+    layout = wibox.layout.align.horizontal,
+    { -- Left widgets
+      layout = wibox.layout.fixed.horizontal,
+      RC.launcher,
+      s.mytaglist,
+      s.mypromptbox,
+    --   {s.mytaglist,
+    --   widget = wibox.container.background,
+    --   bg = "#282A36",
+    --   --opacity = 1,
+    --   --shape_clip = true,
+    -- },
+
+    },
+    s.mytasklist, -- Middle widget
+      { -- Right widgets
+        layout = wibox.layout.fixed.horizontal,
+        --spacing = 10,
+        spacing_widget = {
+          color  = '#dddddd',
+
+        },
+      ykeyboardlayout,
+      wibox.widget.systray(),
+      volume_widget(),
+      brightness_widget(),
+      batteryarc_widget({
+        show_current_level = true,
+        arc_thickness = 1,
+      }),
+      mytextclock,
+      s.mylayoutbox,
+    },
+  }
+end)
+-- }}}
---
+++ .config/awesome/rc.lua
@@ -0,0 +1,117 @@
+-- If LuaRocks is installed, make sure that packages installed through it are
+-- found (e.g. lgi). If LuaRocks is not installed, do nothing.
+pcall(require, "luarocks.loader")
+
+-- Standard awesome library
+local gears = require("gears")
+local awful = require("awful")
+
+-- Theme handling library
+local beautiful = require("beautiful")
+
+-- Miscellanous awesome library
+local menubar = require("menubar")
+
+RC = {} -- global namespace, on top before require any modules
+RC.vars = require("main.user-variables")
+
+-- {{{ Error handling -- }}}
+require("main.error-handling")
+
+-- {{{ Variable definitions
+-- Themes define colours, icons, font and wallpapers.
+beautiful.init(gears.filesystem.get_configuration_dir () .. "theme/theme.lua")
+beautiful.wallpaper = RC.vars.wallpaper
+-- }}}
+
+modkey = RC.vars.modkey
+
+-- Custom Local Library
+local main = {
+  layouts = require("main.layouts"),
+  tags    = require("main.tags"),
+  menu    = require("main.menu"),
+  rules   = require("main.rules"),
+}
+
+-- Custom Local Library: Keys and Mouse Binding
+local binding = {
+  globalbuttons = require("binding.globalbuttons"),
+  clientbuttons = require("binding.clientbuttons"),
+  globalkeys    = require("binding.globalkeys"),
+  bindtotags    = require("binding.bindtotags"),
+  clientkeys    = require("binding.clientkeys")
+}
+
+-- {{{ Layouts
+-- Table of layouts to cover with awful.layout.inc, order matters.
+-- a variable needed in main.tags, and statusbar
+-- awful.layout.layouts = { ... }
+RC.layouts = main.layouts()
+-- }}}
+
+-- {{{ Tags
+-- Define a tag table which hold all screen tags.
+-- a variable needed in rules, tasklist, and globalkeys
+RC.tags = main.tags()
+-- }}}
+
+-- {{{ Menu
+-- Create a laucher widget and a main menu
+RC.mainmenu = awful.menu({ items = main.menu() }) -- in globalkeys
+
+-- a variable needed in statusbar (helper)
+RC.launcher = awful.widget.launcher(
+  { image = beautiful.awesome_icon, menu = RC.mainmenu }
+)
+
+-- Menubar configuration
+-- Set the terminal for applications that require it
+menubar.utils.terminal = RC.vars.terminal
+
+-- }}}
+
+-- {{{ Mouse and Key bindings
+RC.globalkeys = binding.globalkeys()
+RC.globalkeys = binding.bindtotags(RC.globalkeys)
+
+-- Set root
+root.buttons(binding.globalbuttons())
+root.keys(RC.globalkeys)
+-- }}}
+
+-- Keyboard map indicator and switcher
+mykeyboardlayout = awful.widget.keyboardlayout()
+
+-- {{{ Statusbar: Wibar
+require("deco.statusbar")
+-- }}}
+
+-- {{{ Rules
+-- Rules to apply to new clients (through the "manage" signal).
+awful.rules.rules = main.rules(
+  binding.clientkeys(),
+  binding.clientbuttons()
+)
+-- }}}
+
+-- {{{ Signals
+require("main.signals")
+require("awful.autofocus")
+-- }}}
+
+-- startup apps
+--awful.spawn.with_shell("~/.config/awesome/autorun.sh")
+do
+  local cmds =
+  {
+    "picom --experimental-backends ",
+    "vmware-user-suid-wrapper --no--startup-id",
+  }
+
+  for _,i in pairs(cmds) do
+    awful.util.spawn(i)
+  end
+end
+
+
